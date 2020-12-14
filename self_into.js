4c83278454c09fe8a56c2116a57369e5a49c732e
MichiY3 <michiyoy@hotmail.com>
2020-12-14 20:49:35 +0900

    初期セットアップ

---
+++ self_into.js
@@ -0,0 +1,13 @@
+// drawer
+// function toggleDrower(isActive) {
+//     $('.js-hamburger')
+//     if (isActive) {
+//       $('#drower-bg').fadeOut(600);
+//     } else {
+//       $('#drower-bg').fadeIn(600);
+//     }
+//     $('.js-hamburger').toggleClass('on');
+//     $('.js-drower').toggleClass('on');
+//   }
+
+});
