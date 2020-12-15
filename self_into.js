348e42b1312b3c16bee9c9d31e2884a7c02e1795
MichiY3 <michiyoy@hotmail.com>
2020-12-15 17:58:40 +0900

    写真の追加・位置決め、動きの追加（css）,

--- self_into.js
+++ self_into.js
@@ -1,13 +1,19 @@
-// drawer
-// function toggleDrower(isActive) {
-//     $('.js-hamburger')
-//     if (isActive) {
-//       $('#drower-bg').fadeOut(600);
-//     } else {
-//       $('#drower-bg').fadeIn(600);
-//     }
-//     $('.js-hamburger').toggleClass('on');
-//     $('.js-drower').toggleClass('on');
-//   }
+$(function(){
+    // $('.animal_txt').click(function(){
+    //         $('#bear_img').fadeIn(1000);
+    //         });
+    // $('#bear_img').click(function() {
+    //         $('#bear_img').fadeOut(1200);
+    //         });
+
+// 表示ボタンが押された時
+$('.q10-show-btn').on('click',function(){
+	$('#bear_img').fadeIn(1000);
+});
+// 非表示ボタンが押された時、
+$('.q10-hide-btn').on('click',function(){
+	$('#bear_img').fadeOut(1000);
+});
+
 
 });
