163e175a82514f59bf37a65978bf4f3c6e66b2e1
MichiY3 <michiyoy@hotmail.com>
2020-12-17 11:50:55 +0900

    下の方ついか　途中だけどとりあえず

--- self_into.js
+++ self_into.js
@@ -1,19 +1,33 @@
 $(function(){
-    // $('.animal_txt').click(function(){
-    //         $('#bear_img').fadeIn(1000);
-    //         });
-    // $('#bear_img').click(function() {
-    //         $('#bear_img').fadeOut(1200);
-    //         });
+    
+// ページの最上部に戻る
+$('.arrow_top').on('click',function(){
+	// アニメーションをつけたい要素.animate({アニメーションの中で行いたい処理},アニメーション間隔：０.１秒)
+	// bodyと htmlの両方に書くことで、ブラウザ依存を防ぐ
+	// （使っているブラウザで働きが変わらないように）
+	$('body , html').animate({scrollTop:0},1000);
 
-// 表示ボタンが押された時
-$('.q10-show-btn').on('click',function(){
-	$('#bear_img').fadeIn(1000);
-});
-// 非表示ボタンが押された時、
-$('.q10-hide-btn').on('click',function(){
-	$('#bear_img').fadeOut(1000);
 });
 
+       
+    
+// Q8 モーダル　Modal
+	// モーダル表示
+	// js-modalが押されたら２個が表示される
+    $('.modal_open').on('click',function(){
+        $('.modal-content').fadeIn('slow'); 
+                                    // 遅めのアニメーション
+        $('#modal-bg').fadeIn('slow');
+    })
+    
+        // モーダル非表示
+        // modal-closeが押されたら、２個が消える
+    $('.close-btn').on('click',function(){
+        $('.modal-content').fadeOut('slow'); 
+                                    // 遅めのアニメーション
+        $('#modal-bg').fadeOut('slow');
+    
+    
+    });
 
 });
